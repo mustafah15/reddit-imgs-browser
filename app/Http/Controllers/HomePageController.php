264521e27829bad020ac4a2572e1b83c8b9c8af4
mustafah15 <mustafa.hussain93@gmail.com>
2017-01-19 09:50:22 +0200

    finishing front

---
+++ app/Http/Controllers/HomePageController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 1/14/17
+ * Time: 8:08 AM
+ */
+
+namespace App\Http\Controllers;
+
+
+class HomePageController
+{
+
+}
---
+++ app/Managers/PicsParserManger.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 1/14/17
+ * Time: 9:32 AM
+ */
+
+namespace App\Managers;
+
+
+class PicsParserManger
+{
+
+}
